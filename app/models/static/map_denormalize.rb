2243af333bb7c8388e98e7d08d393a044b59695b
David Briggs <godamonra@gmail.com>
2015-06-23 11:09:22 -0400

    disable read only records for static table

--- app/models/static/map_denormalize.rb
+++ app/models/static/map_denormalize.rb
@@ -2,7 +2,7 @@ module Static
   class MapDenormalize < StaticDatabase
     self.table_name = "mapDenormalize"
     self.primary_key = "itemID"
-    include ReadOnlyRecord
+    # include ReadOnlyRecord
 
     alias_attribute :name, :itemName
     alias_attribute :group_id, :groupID
